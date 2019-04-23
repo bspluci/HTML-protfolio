a30c3776c8697b4f5382bff2823ed6cf258271a9
bspluci <47733415+bspluci@users.noreply.github.com>
2019-04-24 01:12:59 +0900

    04.24.01.12

--- kakaofriends/js/custom.js
+++ kakaofriends/js/custom.js
@@ -1,14 +1,19 @@
 ﻿$(document).ready(function(){
     $(document).scroll(function(){
         var scrolltop = $(window).scrollTop();
-        if(scrolltop >= $(".section").offset().top -1000){
+        if(scrolltop >= $(".sectiona").offset().top -800){
             $(".sectionaimg01").addClass("ani01");
             $(".sectionaimg02").addClass("ani02");
             $(".sectionaimg03").addClass("ani03");
             $(".sectionaimg04").addClass("ani04");
         }
-        if(scrolltop >= $(".section").offset().top){
+        if(scrolltop >= $(".sectionb").offset().top -800){
             $(".sectionbimg01").addClass("ani05");
+            $(".sectionbimg02").addClass("ani06");
+        }
+        if(scrolltop >= $(".sectionc").offset().top -500){
+            $(".sectioncimg01").addClass("ani07");    
+            $(".sectioncimg02").addClass("ani08");    
         }
     });
 })
