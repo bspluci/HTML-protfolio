5b9df7980e317d8b3e8df048fdd24886c53c2ecf
parkjeongho <bspluci@gmail.com>
2019-04-23 17:20:06 +0900

    04.23.05.20

---
+++ kakaofriends/js/custom.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function(){
+    $(document).scroll(function(){
+        var scrolltop = $(window).scrollTop();
+        if(scrolltop >= $(".section").offset().top -1000){
+            $(".sectionaimg01").addClass("ani01");
+            $(".sectionaimg02").addClass("ani02");
+            $(".sectionaimg03").addClass("ani03");
+            $(".sectionaimg04").addClass("ani04");
+        }
+        if(scrolltop >= $(".section").offset().top){
+            $(".sectionbimg01").addClass("ani05");
+        }
+    });
+})
