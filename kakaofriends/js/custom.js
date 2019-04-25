d53c29568863cb97c3fd09abc2614b4287af6bd3
bspluci <47733415+bspluci@users.noreply.github.com>
2019-04-26 02:03:07 +0900

    04.26.02.03

--- kakaofriends/js/custom.js
+++ kakaofriends/js/custom.js
@@ -6,21 +6,21 @@
         }else{
             $(".menubox").removeClass("menushadow");
         }
-        if(scrolltop >= $(".sectiona").offset().top -800){
+        if(scrolltop >= $(".sectiona").offset().top -700){
             $(".sectionaimg01").addClass("ani01");
             $(".sectionaimg02").addClass("ani02");
             $(".sectionaimg03").addClass("ani03");
             $(".sectionaimg04").addClass("ani04");
         }
-        if(scrolltop >= $(".sectionb").offset().top -800){
+        if(scrolltop >= $(".sectionb").offset().top -700){
             $(".sectionbimg01").addClass("ani05");
             $(".sectionbimg02").addClass("ani06");
         }
-        if(scrolltop >= $(".sectionc").offset().top -800){
+        if(scrolltop >= $(".sectionc").offset().top -700){
             $(".sectioncimg01").addClass("ani07");    
             $(".sectioncimg02").addClass("ani08");    
         }
-        if(scrolltop >= $(".sectiond").offset().top -800){
+        if(scrolltop >= $(".sectiond").offset().top -700){
             $(".sectiondcard>img").addClass("ani10");
             $(".cardfriends01, .cardfriends02, .cardfriends03, .cardfriends04").addClass("ani11");
         }
