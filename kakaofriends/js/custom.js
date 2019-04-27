7f67d922cfc2f799c2ba29296543324879bf0dbc
bspluci <47733415+bspluci@users.noreply.github.com>
2019-04-28 02:10:33 +0900

    04.28.02.10

--- kakaofriends/js/custom.js
+++ kakaofriends/js/custom.js
@@ -1,4 +1,5 @@
 ﻿$(document).ready(function(){
+//    스크롤 높이에따른 애니메이션 시작 이벤트
     $(document).scroll(function(){
         var scrolltop = $(window).scrollTop();
         if(scrolltop > 0){
@@ -25,6 +26,8 @@
             $(".cardfriends01, .cardfriends02, .cardfriends03, .cardfriends04").addClass("ani11");
         }
     });
+    
+//    메인 카드 애니메이션
     $(".ad02, .ad03, .ad04").hide();
     $(".thumbimgbox>img").click(function(){
         var i = $(this).index();
@@ -32,17 +35,81 @@
         $(".cardbox>div").eq(i).fadeIn();
     });
     
-    $(".sectbheader>ul>li").hide();
-    $(".up").hide();
-    $(".down").show();
-    $(".sectbheader>ul").click(function(){
-        $(".sectbheader>ul>li").slideToggle();
-        $(".up").toggle();
-        $(".down").toggle();
+//    프렌즈샵 슬라이더2개
+    $(".slide01>li, .slide02>li").hide();
+    $(".up, .upp").hide();
+    $(".down, .downn").show();
+    $(".slide01").click(function(){
+        $(".slide01>li").slideToggle();
+        $(".up, .down").toggle();
     });
-        $(".showtext").html("전체 캐릭터");
-    $(".sectbheader>ul>li").click(function(){
+        $(".showtext").html("신상품순");
+    $(".slide01>li").click(function(){
         var thumbtext = $(this).html();
         $(".showtext").html(thumbtext);    
     });
+    
+    $(".slide02").click(function(){
+        $(".slide02>li").slideToggle();
+        $(".upp, .downn").toggle();
+    });
+        $(".showtextt").html("전체 캐릭터");
+    $(".slide02>li").click(function(){
+        var thumbtext = $(this).html();
+        $(".showtextt").html(thumbtext);    
+    });
+
+//    프렌즈샵 페이지넘버 배경
+    $(".sectcpage>p").click(function(){
+        $(".sectcpage>p").removeClass("pageon");
+        $(this).addClass("pageon");    
+    });
+    
+//    스토어 지역클릭 이벤트
+    $(".city>ul>li").click(function(){
+        $(".city>ul>li").removeClass("cityon");
+        $(".city>ul>li>a").css({"color":"#7d7d7d"});
+        $(this).addClass("cityon");
+        $(this).find("a").css({"color":"#000"});
+    });
+    $("#allcity").click(function(){
+        $(".sectbline>div").show();
+    });
+    $("#seoul").click(function(){
+        $(".sectbline>div").hide();
+        $(".sectbline>div.seoul").show();    
+    });
+    $("#gyunggi").click(function(){
+        $(".sectbline>div").hide();
+        $(".sectbline>div.gyunggi").show();    
+    });
+    $("#inchon").click(function(){
+        $(".sectbline>div").hide();
+        $(".sectbline>div.inchon").show();    
+    });
+    $("#daegu").click(function(){
+        $(".sectbline>div").hide();
+        $(".sectbline>div.daegu").show();    
+    });
+    $("#busan").click(function(){
+        $(".sectbline>div").hide();
+        $(".sectbline>div.busan").show();    
+    });
+    $("#jeju").click(function(){
+        $(".sectbline>div").hide();
+        $(".sectbline>div.jeju").show();    
+    });
+    $("#daejun").click(function(){
+        $(".sectbline>div").hide();
+        $(".sectbline>div.daejun").show();    
+    });
 });    
+
+
+
+
+
+
+
+
+
