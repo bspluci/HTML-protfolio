4723ea7bef1876cd22ef0077369e623c4a0925f9
bspluci <47733415+bspluci@users.noreply.github.com>
2019-05-10 09:50:06 +0900

    05.10.09.50

---
+++ ChinaConnect/js/custom.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function(){
+    $(".configsectionbox02, .configsectionbox03").hide();
+    $(".underline").css({"background":"#fff", "border-bottom":"none", "color":"#ff0000"});
+    $(".configtitle>div").click(function(){
+        var i = $(this).index();
+        $(".configtitle>div").css({"background":"#fbfcfc", "border-bottom":"2px solid #d7d7d7", "color":"#000"});
+        $(".configtitle>div").removeClass("underline");
+        $(".configtitle>div").eq(i).addClass("underline");
+        $(".configtitle>div").eq(i).css({"background":"#fff", "border-bottom":"none", "color":"#ff0000"});
+        $(".configsectionbox").hide();
+        $(".configsection>div").eq(i).show();
+    });    
+});
