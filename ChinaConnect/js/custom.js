cc98328184c64cfc504c1cf54aa3f62fba2ac0f6
bspluci <47733415+bspluci@users.noreply.github.com>
2019-05-11 12:20:46 +0900

    05.11.12.20

--- ChinaConnect/js/custom.js
+++ ChinaConnect/js/custom.js
@@ -1,14 +1,19 @@
 ﻿$(document).ready(function(){
     $(".configsectionbox02, .configsectionbox03").hide();
-    $(".underline").css({"background":"#fff", "border-bottom":"none", "color":"#ff0000"});
     $(".configtitle>div").click(function(){
         var i = $(this).index();
-        $(".configtitle>div").css({"background":"#fbfcfc", "border-bottom":"2px solid #d7d7d7", "color":"#000"});
-        $(".configtitle>div").removeClass("underline");
-        $(".configtitle>div").eq(i).addClass("underline");
-        $(".configtitle>div").eq(i).css({"background":"#fff", "border-bottom":"none", "color":"#ff0000"});
+        $(".configtitle>div").removeClass("underline tabon");
+        $(".configtitle>div").eq(i).addClass("underline tabon");
         $(".configsectionbox").hide();
         $(".configsection>div").eq(i).show();
+    });
+    $(".blogtab").hide();
+    $(".nbtitle>div").click(function(){
+        var i = $(this).index();
+        $(".nbtitle>div").removeClass("underline tabon");
+        $(".nbtitle>div").eq(i).addClass("underline tabon");
+        $(".nbsection>div").hide();
+        $(".nbsection>div").eq(i).show();
     });
     $(".menu01").click(function(){
         $("html, body").stop().animate({scrollTop:$(".connectintro").offset().top -120}, 800);
